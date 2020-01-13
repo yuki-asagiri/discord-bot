a7efce6534ed61bbea027275175a74afbe641404
hojyo <laruf.5.brack@gmail.com>
2020-01-13 15:47:48 +0900

    discord bot first

---
+++ file_cont.py
@@ -0,0 +1,129 @@
+import requests
+import glob
+import json
+import urllib.request
+import os
+
+def test():
+    print('直接読まないで')
+
+def list_in_message(message, lolist):
+    for i in lolist:
+        if i in message:
+            return True
+
+def list_in_ob(message, lolist):
+    for i in lolist:
+        if i in message:
+            return i
+
+def status_converter(status, name, val):
+    chara = chara_loader(name)
+    chara['status'][status] = val
+
+    f = open('path/chara_data/'+chara["name"]+'.json', 'w')
+    json.dump(chara, f)   
+
+    return chara
+
+def chara_loader(name):
+    file_path = 'path/chara_data/'
+    chara_name = str(name) + '.json'
+    data_path = file_path + chara_name
+
+    with open(data_path) as f:
+        s = f.read()
+    result = json.loads(s)
+    return result
+
+def chara_lister():
+    dir_path = 'path/chara_data/'
+    file_list = glob.glob(dir_path+'*')
+    pc_name_list = []
+    for file_name in file_list:
+       pc_name_list.append(file_name.split('.')[0].split('/')[2]) 
+    print(pc_name_list)
+
+    return pc_name_list
+
+def chara_data_download(id_url):
+    print('download読み込み')
+    chara_id = 'path/chara_data/malmalmalmal.json'
+    
+    # return urllib.request.urlretrieve(id_url, chara_id)
+    response = requests.get(id_url)
+    print(response)
+    chara_data = urllib.request.urlretrieve(id_url, chara_id)
+
+    return chara_data
+
+def chara_data_extracter():
+    
+    with open('path/chara_data/malmalmalmal.json') as f:
+        s = f.read()
+    chara = json.loads(s)
+    new_chara_json = { "name" : chara["pc_name"],
+      "status" : {
+          "STR"  : chara["NA1"],
+          "CON"  : chara["NA2"],
+          "POW"  : chara["NA3"],
+          "DEX"  : chara["NA4"],
+          "APP"  : chara["NA5"],
+          "SIZ"  : chara["NA6"],
+          "INT"  : chara["NA7"],
+          "EDU"  : chara["NA8"],
+          "HP"   : chara["NA9"],
+          "MP"   : chara["NA10"],
+          "SAN"  : chara["NA11"],
+          "idea" : chara["NA12"],
+          "幸運" : chara["NA13"],
+          "知識" : chara["NA14"]
+        }
+    } 
+
+    f = open('path/chara_data/'+new_chara_json["name"]+'.json', 'w')
+    json.dump(new_chara_json, f)
+    
+    os.remove('path/chara_data/malmalmalmal.json')
+
+    return new_chara_json
+
+def chara_data_output(chara_name, form):
+    name = chara_name
+    dir_path = 'path/chara_data/'
+    file_path = dir_path + name + '.json'
+    if not os.path.isfile(file_path):
+        return 'キャラクターがいません'   
+    else:
+        with open(file_path, 'r') as f:
+            s = f.read()
+        chara_data = json.loads(s)
+        return outputer(chara_data, form)
+
+def outputer(data, form):
+    if form == 'full':
+        put_result = (  'PC名 '+data['name']+'\n'
+                        'STR  '+data['status']['STR']+'\n'
+                        'CON  '+data['status']['CON']+'\n'
+                        'POW  '+data['status']['POW']+'\n'
+                        'DEX  '+data['status']['DEX']+'\n'
+                        'APP  '+data['status']['APP']+'\n'
+                        'SIZ  '+data['status']['SIZ']+'\n'
+                        'INT  '+data['status']['INT']+'\n'
+                        'EDU  '+data['status']['EDU']+'\n'
+                        'HP   '+data['status']['HP']+'\n'
+                        'MP   '+data['status']['MP']+'\n'
+                        'SAN  '+data['status']['SAN']+'\n'
+                        'idea '+data['status']['idea']+'\n'
+                        '幸運 '+data['status']['幸運']+'\n'
+                        '知識 '+data['status']['知識'])
+    else:
+        status = form
+        put_result = (  'PC名 '+data['name']+'\n'+status + data['status'][form])
+    print(put_result)
+    return put_result
+
+if __name__ == '__main__':
+    test()
+
+print('success file load')
---
+++ main.py
@@ -0,0 +1,105 @@
+import discord
+import asyncio
+import file_cont
+
+client = discord.Client()
+global read_flag
+global oumu_flag
+global pc_list
+global status_list
+global name_flag
+global status_flag
+
+name_flag = 'No Chara'
+read_flag = 'Off'
+oumu_flag = 'Off'
+pc_list = file_cont.chara_lister()
+status_flag = 'No Status'
+status_list = ['STR', 'CON', 'POW', 'DEX', 'APP', 'SIZ', 'INT', 'EDU', 'HP', 'MP', 'SAN', 'idea', '幸運', '知識']
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    global name_flag
+    global oumu_flag
+    global read_flag
+    global pc_list
+    global status_list
+    if message.author == client.user:
+        return
+
+    if oumu_flag == 'On':
+        oumu_flag = 'Off'
+        await message.channel.send(message.content)
+
+    if '御中くんさよなら' in message.content:
+        msg = '皆さんさようならっす'
+        oumu_flag = 'Off'
+        await message.channel.send(msg)
+
+    if '返事して' in message.content:
+        msg = '了解っす'
+        oumu_flag = 'On'
+        await = message.channel.send(msg)
+
+    if 'Hello' in message.content or '御中くんこんにちは':
+        print(message.author.name)
+        msg = "こんにちはっす" + message.author.name + "さん"
+        await message.channel.send(msg)
+
+    if 'ニラレバ' in message.content:
+        print('ニラレバ開始')
+        msg = "キャラシのURLを貼ってね"
+        read_flag = 'On'
+        print(read_flag)
+        await message.channel.send(msg)
+
+    if 'http' in message.content:
+        print('read_flag')
+        print(read_flag)
+        if read_flag == 'On':
+            print('データ読み込み')
+            print('success!!')
+            chara_url = message.content + '.js'
+            print(chara_url)
+            chara_data = file_cont.chara_data_download(chara_url)
+            print(chara_data)
+            ext_chara = file_cont.chara_data_extracter()
+            await message.channel.send('以下のキャラデータを保存しました')
+            await message.channel.send(file_cont.chara_data_output(ext_chara['name']))
+            pc_list.append(ext_chara['name'])
+            print(pc_list)
+            read_flag = 'Off'
+
+    if file_cont.list_in_message(message.content, pc_list):
+        msg = "以下のキャラクターを読み込みました" 
+        await message.channel.send(msg)
+        print(message.content)
+        name_flag = message.content 
+        await message.channel.send(file_cont.chara_data_output(message.content, 'full'))
+
+    if file_cont.list_in_message(message.content, status_list) and ':' in message.content:
+        msg = "ステータス更新"
+        status_flag = message.content.split(':')
+        print(status_flag)
+        await message.channel.send(msg)
+        await message.channel.send(name_flag+'の'+status_flag[0]+'を'+status_flag[1]+'に更新します')
+        new_chara_data = file_cont.status_converter(status_flag[0], name_flag, status_flag[1])
+        await message.channel.send(file_cont.chara_data_output(name_flag, 'full'))
+        name_flag = 'No Chara'
+        status_flag = 'No Status'
+
+    if 'ステータス表示' in message.content and file_cont.list_in_message(message.content, status_list):
+        status_flag = file_cont.list_in_ob(message.content, status_list)
+        msg = name_flag + 'の' + status_flag + 'を表示します'
+        await message.channel.send(file_cont.chara_data_output(name_flag, status_flag))
+        status_flag = 'No Status'
+
+client.run("NjY1ODA4MTAxOTUzNTAzMjQy.XhrAZQ.JXE6A5bE5eRMfwqN6MgrufuXWHI")
+
