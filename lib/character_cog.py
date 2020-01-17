36ae45fc6a5f3e0e160bccad70811f0ecb9b1af9
Yuki Asagiri <asagiri.detectiveoffice@gmail.com>
2020-01-17 22:38:02 +0900

    fix CharaCog class

--- lib/character_cog.py
+++ lib/character_cog.py
@@ -8,13 +8,16 @@ class CharaCog(commands.Cog):
     def __init__(self, bot, pc_unique_id):
         self.bot = bot
         self.pc_unique_id = pc_unique_id
+        commands = self.get_commands()
+        for c in commands:
+            c.uopdate(name = self.pc_unique_id)
         print('setup command $' + pc_unique_id)
 
     # コマンドグループのルート
     # ${unique_id} に対応
-    @commands.group(name = self.pc_unique_id)
+    @commands.group()
     async def root(self, ctx):
         # サブコマンドが呼ばれていない場合、メッセージを表示
         if ctx.invoked_subcommand is None:
-            chara = cc.get_chara_data(pc_unique_id)
+            chara = cc.get_chara_data(self.pc_unique_id)
             await ctx.send( chara['name'] + ' のコマンドっす。\n このコマンドにはサブコマンドが必要っす。')
